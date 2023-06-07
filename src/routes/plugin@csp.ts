5de1f5b7dadf72d897f8207b8b00b674972f16d3
Tobias Zimmermann <zimmermann@sport-thieme.de>
2023-06-07 09:27:36 +0100

    added csp report only

---
+++ src/routes/plugin@csp.ts
@@ -0,0 +1,19 @@
+import type { RequestHandler } from "@builder.io/qwik-city";
+
+export const onRequest: RequestHandler = ev => {
+  const nonce = Date.now().toString(36);
+  ev.sharedMap.set("@nonce", nonce);
+  const csp = [
+    `default-src 'self' 'unsafe-inline'`,
+    `font-src 'self'`,
+    `img-src 'self' 'unsafe-inline' data:`,
+    `script-src 'strict-dynamic' 'unsafe-inline' 'nonce-${nonce}'`,
+    `style-src 'self' 'unsafe-inline'`,
+    `frame-src 'self' 'nonce-${nonce}'`,
+    `object-src 'none'`,
+    `base-uri 'self'`,
+    `require-trusted-types-for 'script'`,
+  ];
+
+  ev.headers.set("Content-Security-Policy-Report-Only", csp.join("; "));
+};
